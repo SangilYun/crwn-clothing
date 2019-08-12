643c015aefd9fba41cb4ed76a2a2c56b9e12ef3f
SangilYun <nasuler@gmail.com>
2019-08-13 00:12:48 +0900

    update the app with a shop page, collection preview component and collection item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 
-import './directory.style.scss';
+import './directory.styles.scss';
 
 class Directory extends React.Component{
     constructor(){
