2ca0c2708679dfdd21087c2b51152b91d7a98b5c
SangilYun <nasuler@gmail.com>
2019-08-19 00:21:28 +0900

    moved our directory and shop data into its own reducer, created respective selector  and updated corresponding component with new redux flow for  directory/shop

--- src/components/collections-preview/collections-preview.component.jsx
+++ src/components/collections-preview/collections-preview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
-import './collection-preview.styles.scss';
+import './collections-preview.styles.scss';
 
 const CollectionPreview = ({title, items}) => (
     <div className ='collection-preview'>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-class ShopPage extends React.Component{
-    constructor(){
-        super();
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-        this.state = {
-            collections:SHOP_DATA
-        }
-    }
-    render(){
-        const {collections} = this.state;
-        return (
-              <div className='shop-page'>
-                  {
-                      collections.map(({id, ...otherCollectionProps})=>(
-                          <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-                      ))
-                  }
-              </div>
-        );
-    }
-} 
+const ShopPage =({ match })=>(
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+    </div>
+)
+
 
 export default ShopPage;
